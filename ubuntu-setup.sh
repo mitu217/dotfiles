e99297791a980a1ae6df8bd11a029a9aa0658612
Mitu217 <mtksk.26884um@gmail.com>
2017-03-25 10:35:11 +0900

    ubuntuのインストールスクリプト追加

---
+++ ubuntu-setup.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# --------------------
+# Ubuntu Setup Scripts
+# Author: Mitu217
+# Update: 2017/03/20
+# --------------------
+
+# Check　Permission
+if [[ $EUID -ne 0 ]]; then
+    echo 'Please run it as an administrator.'
+    exit;
+fi
+
+# Copy dotfiles
+if [ -e ~/.zshrc ]; then
+  rm ~/.zshrc
+fi
+echo 'source ~/dotfiles/.zshrc' > ~/.zshrc
+source ~/.zshrc
+zsh
+
+if [ -e ~/.tmux.conf ]; then
+  rm ~/.tmux.conf
+fi
+echo 'source ~/dotfiles/.tmux.conf' > ~/.tmux.conf
+
+if [ -e ~/.vimrc ]; then
+  rm ~/.vimrc
+fi
+echo 'source ~/dotfiles/.vimrc' > ~/.vimrc
+
+
+# Add PPA
+sudo add-apt-repository ppa:webupd8team/atom
+
+# Update Apt-get
+sudo apt-get update
+
+# Install Middlewares
+sudo apt-get install -y git
+sudo apt-get install -y vim
+sudo apt-get install -y tmux
+sudo apt-get install -y zsh
+sudo apt-get install -y Virtualbox
+sudo apt-get install -y Vagrant
+sudo apt-get install -y atom
+sudo apt-get install -y tilda
+
+
+# ---------------
+# Setting Options
+# ---------------
+
+# Neobundle
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
+sh ./install.sh
+rm install.sh
+
+# Nodebrew
+wget git.io/nodebrew
+perl nodebrew setup
+nodebrew install latest
+rm nodebrew
+
+# Change DirectoryName
+LANG=C xdg-user-dirs-gtk-update
+
