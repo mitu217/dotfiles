874d797c382eec8fabaf2f1df0817660df48dfdf
Mitu217 <mtksk.26884um@gmail.com>
2019-01-26 00:05:17 +0900

    UPDATE

--- etc/init/osx/50_vim.sh
+++ etc/init/osx/50_vim.sh
@@ -1,4 +1,16 @@
+#!/bin/bash
 
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-sh ./installer.sh ~/.vim/bundles
-rm install.sh
+# Stop script if errors occur
+trap 'echo Error: $0:$LINENO stopped; exit 1' ERR INT
+set -eu
+
+# Load vital library that is most important and
+# constructed with many minimal functions
+. "$DOTPATH"/etc/lib/vital.sh
+
+# Install dein.vim
+if [ -e ~/.vim/bundles ]; then
+    log_pass "dein.vim: already installed"
+else
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh)" -- ~/.vim/bundles
+fi
