4492ac8c01b5da8a355107405418e93c5486eabd
mitu217 <mtksk.26884um@gmail.com>
2019-10-20 00:25:20 +0900

    Update and rename 40_appimage.sh to 30_appimage.sh

---
+++ etc/init/linux/30_appimage.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Stop script if errors occur
+trap 'echo Error: $0:$LINENO stopped; exit 1' ERR INT
+set -eu
+
+# Load vital library that is most important and
+# constructed with many minimal functions
+. "$DOTPATH"/etc/lib/vital.sh
+
+# This script is only supported with Linux
+if ! is_linux; then
+    log_fail "error: this script is only supported with linux"
+    exit 1
+fi
+
+cd ~/
+
+# vscode
+if has "code"; then
+    log_pass "code: already installed"
+else
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+    sudo apt install apt-transport-https
+    sudo apt update
+    sudo apt install code
+
+    log_pass "code: successfully"
+fi
+
+# bitwarden
+sudo snap install bw bitwarden
+log_pass "bitwarden: successfully"
+
+#brave
+sudo snap install brave
+log_pass "brave: successfully"
+
