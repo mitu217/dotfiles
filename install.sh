a8691a7e3bedace41519736369ae4dbc4c313052
mitu217 <mtksk.26884um@gmail.com>
2019-10-19 12:50:46 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,28 +2,42 @@
 
 set -e
 
-DOT_DIRECTORY="${HOME}/dotfiles"
-DOT_TARBALL="https://github.com/Mitu217/dotfiles/tarball/master"
 REMOTE_URL="git@github.com:Mitu217/dotfiles.git"
+DOTFILES_DIRECTORY="${HOME}/dotfiles"
+DOTFILES_TARBALL="https://github.com/Mitu217/dotfiles/tarball/master"
+DOTFILES=".zshrc .zshenv .vimrc .vimrc.dein"
 
 has() {
   type "$1" > /dev/null 2>&1
 }
 
-if [ ! -d ${DOT_DIRECTORY} ]; then
-  echo "Downloading dotfiles..."
-  mkdir ${DOT_DIRECTORY}
+# download dotfiles
+if [ ! -d ${DOTFILES_DIRECTORY} ]; then
+  echo "==> Downloading dotfiles..."
+  echo ''
+  mkdir ${DOTFILES_DIRECTORY}
 
   if type "git" > /dev/null 2>&1; then
-    git clone --recursive "${REMOTE_URL}" "${DOT_DIRECTORY}"
+    git clone --recursive "${REMOTE_URL}" "${DOTFILES_DIRECTORY}"
   else
-    curl -fsSLo ${HOME}/dotfiles.tar.gz ${DOT_TARBALL}
-    tar -zxf ${HOME}/dotfiles.tar.gz --strip-components 1 -C ${DOT_DIRECTORY}
+    wget -q ${DOTFILES_TARBALL} -O ${HOME}/dotfiles.tar.gz
+    tar -zxf ${HOME}/dotfiles.tar.gz --strip-components 1 -C ${DOTFILES_DIRECTORY}
     rm -f ${HOME}/dotfiles.tar.gz
   fi
 
   echo $(tput setaf 2)Download dotfiles complete!. ✔︎$(tput sgr0)
+  echo ''
 fi
 
-cd ${DOT_DIRECTORY}
-make install
+cd ${DOTFILES_DIRECTORY}
+
+# deploy dotfiles
+echo '==> Start to deploy dotfiles to home directory'
+echo ''
+for val in ${DOTFILES}
+do
+  ln -sfnv ${DOTFILES_DIRECTORY}/${val} ${HOME}/${val}
+done
+echo ''
+echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)
+echo ''
